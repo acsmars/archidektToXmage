201bb8d4f5916b3a6f9e38fa1bf5ef3a805572a4
Austin <acsmars@gmail.com>
2019-09-25 10:57:48 -0700

    Added support for downloading all decks by a user. Fixed issues relating to private decks and decks with illegal characters (for file paths) in their names.

--- archidektToXmage.py
+++ archidektToXmage.py
@@ -3,6 +3,8 @@ import requests
 import os
 import argparse
 
+deckDirectory = "decks"
+
 
 class Card:
     quantity = 0
@@ -20,50 +22,82 @@ class Card:
                                                                                      self.quantity)
 
 
+def downloadDeck(deckID):
+    print("Downloading deck with ID {}".format(deckID))
+
+    # deck api endpoint
+    URL = "https://archidekt.com/api/decks/{}/".format(deckID)
+
+    try:
+        data = requests.get(URL).json()
+    except:
+        print("Exception getting deck {}. It likely doesn't exist, is private, or archidekt is down.")
+        return
+
+    deckName = data.get("name")
+
+    mainboard = []
+    sideboard = []
+
+    if data.get("cards") == None:
+        print("Deck is either private or contains no cards, skipping.")
+        return
+
+    for card in data.get("cards"):
+        newCard = Card()
+
+        newCard.name = card.get("card").get("oracleCard").get("name").split(" //")[0]
+        newCard.quantity = card.get("quantity")
+        newCard.setCollectorNumber = card.get("card").get("collectorNumber")
+        newCard.setCode = card.get("card").get("edition").get("editioncode").upper()
+
+        category = card.get("category")
+        if (category == "Commander"):
+            sideboard.append(newCard)
+        elif (category == "Maybeboard"):
+            pass
+        else:
+            mainboard.append(newCard)
+
+    if not os.path.exists(deckDirectory):
+        os.makedirs(deckDirectory)
+
+    deckFilename = "".join([c for c in deckName if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
+
+    with open("{}/{}.dck".format(deckDirectory, deckFilename), "w") as deckFile:
+        for card in mainboard:
+            deckFile.write(card.xmageFormat() + os.linesep)
+
+        for card in sideboard:
+            deckFile.write("SB: {}".format(card.xmageFormat()) + os.linesep)
+
+
+def listDecks(userID):
+    # user api endpooint
+    URL = "https://archidekt.com/api/users/{}/decks/".format(userID)
+
+    try:
+        data = requests.get(URL).json()
+    except:
+        print("Exception getting deck {}. It likely doesn't exist, is private, or archidekt is down.")
+        return
+    deckIDs = [x.get("id") for x in data.get("decks")]
+    print("Found decks:{}".format(deckIDs))
+    return deckIDs
+
+
 # parse input
 parser = argparse.ArgumentParser(description='Public Deck ID')
-parser.add_argument("deckID")
+parser.add_argument("resourceID")
+parser.add_argument('--user', dest='mode', action='store_const',
+                    const="user", default="deck",
+                    help='Fetch all decks by this user rather than a deck ID')
 args = parser.parse_args()
-print("Downloading deck with ID {}".format(args.deckID))
-
-# api-endpoint
-
-deckDirectory = "decks"
-URL = "https://archidekt.com/api/decks/{}/".format(args.deckID)
-
-try:
-    data = requests.get(URL).json()
-except:
-    print("Exception getting deck {}. It likely doesn't exist, is private, or archidekt is down.")
-    exit(1)
-
-deckName = data.get("name")
-
-mainboard = []
-sideboard = []
-
-for card in data.get("cards"):
-    newCard = Card()
-
-    newCard.name = card.get("card").get("oracleCard").get("name").split(" //")[0]
-    newCard.quantity = card.get("quantity")
-    newCard.setCollectorNumber = card.get("card").get("collectorNumber")
-    newCard.setCode = card.get("card").get("edition").get("editioncode").upper()
-
-    category = card.get("category")
-    if (category == "Commander"):
-        sideboard.append(newCard)
-    elif (category == "Maybeboard"):
-        pass
-    else:
-        mainboard.append(newCard)
-
-if not os.path.exists(deckDirectory):
-    os.makedirs(deckDirectory)
-
-with open("{}/{}.dck".format(deckDirectory, deckName), "w") as deckFile:
-    for card in mainboard:
-        deckFile.write(card.xmageFormat() + os.linesep)
-
-    for card in sideboard:
-        deckFile.write("SB: {}".format(card.xmageFormat()) + os.linesep)
+if args.mode == "user":
+    print("Downloading all decks from user with ID {}".format(args.resourceID))
+    for deckID in listDecks(args.resourceID):
+        downloadDeck(deckID)
+elif args.mode == "deck":
+    downloadDeck(args.resourceID)
+else:
+    print("Invalid Argument")
